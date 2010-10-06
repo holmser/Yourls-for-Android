356c6dfdea863d8311c9e9ba33f388822a25b348
unknown <CCAV1@.(none)>
2010-10-06 13:57:28 -0700

    Update to 1.1, added support for Custom YOURLS in preferences.

--- gen/net/holmser/yourls/R.java
+++ gen/net/holmser/yourls/R.java
@@ -10,32 +10,47 @@ package net.holmser.yourls;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int backgroundColor=0x7f060000;
+        public static final int textColor=0x7f060001;
+        public static final int textViewBackground=0x7f060002;
+        public static final int textViewTextColor=0x7f060003;
+    }
     public static final class drawable {
         public static final int hicon=0x7f020000;
         public static final int icon=0x7f020001;
         public static final int yourls=0x7f020002;
     }
     public static final class id {
-        public static final int CustomURL=0x7f050003;
-        public static final int TextView01=0x7f050001;
-        public static final int TextView02=0x7f050000;
-        public static final int myButton=0x7f050004;
-        public static final int myEditText=0x7f050002;
+        public static final int TextView01=0x7f080001;
+        public static final int appTitle=0x7f080000;
+        public static final int myButton=0x7f080003;
+        public static final int myEditText=0x7f080002;
+        public static final int myShareButton=0x7f080004;
+        public static final int preferences=0x7f080006;
+        public static final int share=0x7f080005;
     }
     public static final class layout {
         public static final int main=0x7f030000;
         public static final int relative=0x7f030001;
     }
+    public static final class menu {
+        public static final int menu=0x7f070000;
+    }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int button_dialog_text=0x7f040002;
-        public static final int button_update_text=0x7f040003;
-        public static final int dialog_cancel=0x7f040007;
-        public static final int dialog_ok=0x7f040006;
-        public static final int dialog_text=0x7f040005;
-        public static final int dialog_title=0x7f040004;
-        public static final int hello=0x7f040000;
-        public static final int sound_file=0x7f040008;
-        public static final int title=0x7f040009;
+        public static final int app_name=0x7f050001;
+        public static final int app_title=0x7f05000a;
+        public static final int button_dialog_text=0x7f050002;
+        public static final int button_update_text=0x7f050003;
+        public static final int dialog_cancel=0x7f050007;
+        public static final int dialog_ok=0x7f050006;
+        public static final int dialog_text=0x7f050005;
+        public static final int dialog_title=0x7f050004;
+        public static final int hello=0x7f050000;
+        public static final int settings=0x7f050009;
+        public static final int sound_file=0x7f050008;
+    }
+    public static final class xml {
+        public static final int preferences=0x7f040000;
     }
 }
--- src/net/holmser/yourls/yourls.java
+++ src/net/holmser/yourls/yourls.java
@@ -6,10 +6,9 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URLEncoder;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -18,99 +17,115 @@ import org.apache.http.StatusLine;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
 
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.ClipboardManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+
 public class yourls extends Activity {
-    public boolean gotURL = false;
+	SharedPreferences preferences;    
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        //initialize button and EditText elements
-        final Button myButton = (Button)findViewById(R.id.myButton);
-        final EditText myEditText = (EditText)findViewById(R.id.myEditText);
-        final EditText CustomURL=(EditText)findByViewId(R.id.CustomURL);
-        final Context context = getApplicationContext();
-        final CharSequence text = "Short URL Copied to Clipboard";
-        final int duration = Toast.LENGTH_SHORT;
         
-        //grabs data from Share intent
+        final Button myButton = (Button)findViewById(R.id.myButton);
+        final Button myShorten = (Button)findViewById(R.id.myShareButton);
+        
+        Button myShareButton = (Button)findViewById(R.id.myShareButton);
+        final EditText myEditText = (EditText)findViewById(R.id.myEditText);
         
         if (getIntent().getStringExtra(Intent.EXTRA_TEXT) != null)
         	myEditText.setText(getIntent().getStringExtra(Intent.EXTRA_TEXT));
-        //Shorten URL Button Listener
+        //Shorten URL Button List
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        
+        
+        
         myButton.setOnClickListener(new OnClickListener(){
         	public void onClick(View _view){
         		
-        		if(gotURL == false){
-        			Toast toast = Toast.makeText(context, "Contacting Holmser.net", duration);
-        			toast.show();
-        			myEditText.setText(CreateUrl(myEditText.getText().toString(), CustomURL.getText().toString()));
-        			myButton.setText("Share URL");
-        			toast = Toast.makeText(context, text, duration);
-        			toast.show();
-        			 ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE); 
-        			 clipboard.setText(myEditText.getText().toString());
-        		}else if (gotURL == true){
-        			Intent intent = new Intent(Intent.ACTION_SEND);
-        			intent.setType("text/plain");
-        			intent.putExtra(intent.EXTRA_TEXT, myEditText.getText().toString());
-        			startActivity(Intent.createChooser(intent, getTitle()));
-        		}
+        		Context context = getApplicationContext();
+                  int duration = Toast.LENGTH_SHORT;
+                 
+                if (myEditText.getText().length() != 0)
+                {	
+                Toast toast = Toast.makeText(context, "Contacting Holmser.net", duration);
+    			toast.show();
+    			
+    			String mServer = preferences.getString("yourlsServer", "http://holmser.net/i/");
+    			String mUsername = preferences.getString("username", "holmser");
+    			String mPassword = preferences.getString("password", "elmoandme");
+    			if (validateSettings(mServer, mUsername, mPassword, context)){
+    				myEditText.setText(CreateUrl(myEditText.getText().toString(), mServer, mUsername, mPassword, null));
+    				//Copy text to clipboard
+        			ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE); 
+    			 	clipboard.setText(myEditText.getText().toString());
+    			 	//toast = Toast.makeText(context, text, duration);
+     				//toast.show();
+     			
+    			//hide Shorten Button
+    			 myButton.getHandler().post(new Runnable() {
+    				    public void run() {
+    				        myButton.setVisibility(View.GONE);
+    				    }
+    				});
+    			//Show share button
+    			 myShorten.getHandler().post(new Runnable() {
+ 				    public void run() {
+ 				        myShorten.setVisibility(View.VISIBLE);
+ 				    }
+    			 
+ 				
+    			 });
+    			}
+        	}else {
+            	Toast toast = Toast.makeText(getApplicationContext(), "Please Enter a valid URL", Toast.LENGTH_SHORT);
+    			toast.show();
+            	}	
+                
+                
         	}
-        	
+        
         });
         
-       /* shareURL.setOnClickListener(new OnClickListener(){
+       myShareButton.setOnClickListener(new OnClickListener(){
         	public void onClick(View _view){
+        		Intent intent = new Intent(Intent.ACTION_SEND);
+        		intent.setType("text/plain");
+        		intent.putExtra(Intent.EXTRA_TEXT, myEditText.getText().toString());
+        		startActivity(Intent.createChooser(intent, getTitle()));
         		
-        		if(shortURL.getText()!= null){
-        			
-        		}else shortURL.setText("Please Enter URL");
         		
         	}
-        });*/
+        });
+       
     }
 
 
-	private EditText findByViewId(int customurl) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-
-	public static String CreateUrl(String original, String custom)
+	public static String CreateUrl(String original, String yourlsLocation, String username, String password, String customURL)
 	{
 		String tinyUrl = null;
-		String urlTemplate;
-		
 		try {
-
+			
 			HttpClient client = new DefaultHttpClient();
-			if (custom != null){
-				urlTemplate = "http://holmser.net/i/yourls-api.php?username=holmser&password=elmoandme&action=shorturl&keyword="+custom+"&url=%s";
-				System.out.println(urlTemplate);
-			}
-			else{
-				urlTemplate = "http://holmser.net/i/yourls-api.php?username=holmser&password=elmoandme&action=shorturl&url=%s";
-			}
-			
+			String urlTemplate =yourlsLocation+ "yourls-api.php?username="+username+"&password="+
+				password+"&action=shorturl&url=%s";
 			String uri = String.format(urlTemplate, URLEncoder.encode(original));
-			
-				
 			HttpGet request = new HttpGet(uri);
 			HttpResponse response = client.execute(request);
 			HttpEntity entity = response.getEntity();
@@ -127,7 +142,7 @@ public class yourls extends Activity {
 					tinyUrl = bufferedReader.readLine();
 					if (tinyUrl != null)
 					{
-						System.out.println("Created Url-"+tinyUrl.substring( tinyUrl.indexOf("<shorturl>")+10, tinyUrl.indexOf("</shorturl>")));
+						System.out.println("Created Url-"+tinyUrl);
 					}
 					else {
 						throw new IOException("empty response");
@@ -152,6 +167,40 @@ public class yourls extends Activity {
 		return sub;
 	
 	}
+	 @Override
+	    public boolean onCreateOptionsMenu(Menu menu) {
+	    	MenuInflater inflater = getMenuInflater();
+	    	inflater.inflate(R.menu.menu, menu);
+	    	return true;
+	    }
+	 @Override
+		public boolean onOptionsItemSelected(MenuItem item) {
+			switch (item.getItemId()) {
+			// We have only one menu option
+			case R.id.preferences:
+				// Launch Preference activity
+				Intent i = new Intent(yourls.this, Preferences.class);
+				startActivity(i);
+				// A toast is a view containing a quick little message for the user.
+				Toast.makeText(yourls.this,
+						"Enter your YOURLS settings here",
+						Toast.LENGTH_LONG).show();
+				break;
+
+			}
+			return true;
+		}
+	 public boolean validateSettings(String server, String username, String password, Context context){
+		 if (server == "" || username == "" || password==""){
+			 Toast toast = Toast.makeText(context, "Your settings are invalid, please change them", Toast.LENGTH_LONG);
+ 			toast.show();
+			 //Toast.makeText(yourls.this,
+				//		"Your settings are invalid, please change them",
+					//	Toast.LENGTH_LONG).show();
+			 return false;
+		 }else return true;
+	 }
 	
 }
+
 
